b0d8de42250c9c20e56cda5b65d0034f39fe391f
mostafa-Ali-Elkady <60076767+mostafa-Ali-Elkady@users.noreply.github.com>
2023-08-23 02:25:29 +0300

    Update index.js

--- assignment 8/JS/index.js
+++ assignment 8/JS/index.js
@@ -7,10 +7,10 @@ var quoteAuthor=document.getElementById("quoteAuthor");
 
 function generateQuote() {
 
-    var quotes = [{ quote: `“Be yourself; everyone else is already taken.”`, author: `― Oscar Wilde` },
-    {quote: `“So many books, so little time.”`, author: `― Frank Zappa` },
-    { quote: `“A room without books is like a body without a soul.”`, author: `― Marcus Tullius Cicero` },
-    { quote: `“You only live once, but if you do it right, once is enough.”`, author: `― Mae West` },
+    var quotes = [{ quote: `“Be yourself; everyone else is already taken.”`, author: `― Nasr Gamal` },
+    {quote: `“So many books, so little time.”`, author: `― Nasr Gamal` },
+    { quote: `“A room without books is like a body without a soul.”`, author: `― Nasr Gamal`` },
+    { quote: `“You only live once, but  you  it right, once is enough.”`, author: `― mostafa el kady` },
     ]
 
     var random = Math.trunc(Math.random() * quotes.length);
